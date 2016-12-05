326dc39e8785444f85c383129e7f4b3dfe900ee9
Emma Azelborn <eazellou@users.noreply.github.com>
2016-12-05 12:47:04 -0500

    bit crush initial commit

---
+++ VE1939_bitCrush/Debug/subdir_vars.mk
@@ -0,0 +1,33 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../lnkx.cmd 
+
+C_SRCS += \
+../AIC_func.c \
+../hailMary.c 
+
+OBJS += \
+./AIC_func.obj \
+./hailMary.obj 
+
+C_DEPS += \
+./AIC_func.pp \
+./hailMary.pp 
+
+C_DEPS__QUOTED += \
+"AIC_func.pp" \
+"hailMary.pp" 
+
+OBJS__QUOTED += \
+"AIC_func.obj" \
+"hailMary.obj" 
+
+C_SRCS__QUOTED += \
+"../AIC_func.c" \
+"../hailMary.c" 
+
+
